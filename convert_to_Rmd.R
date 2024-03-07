776603f589e52ad2eef2e975ecf77d262fbde1cd
Jenna T. B. Ekwealor <jtbe@berkeley.edu>
2024-03-07 13:32:04 -0800

    update converted original Rmds

--- convert_to_Rmd.R
+++ convert_to_Rmd.R
@@ -1,4 +1,8 @@
 library(rmarkdown)
+# the next three lines find the script location and set that as the working directory. copy these lines into all scripts for this project. 
+library(rstudioapi)
+wd_script_location <- dirname(rstudioapi::getSourceEditorContext()$path)
+setwd(wd_script_location)
 
 # this script converts html to Rmd
 original <- list.files( pattern = "html.html")
@@ -7,5 +11,5 @@ for (i in 1:length(original)) {
   
 chapter <- gsub("_html.html", "", original[i])
   
-pandoc_convert(original[i], to = "markdown_strict", output = paste0(chapter, "_original.Rmd"))
+pandoc_convert(original[i], to = "markdown", output = paste0(chapter, "_original.Rmd"))
 }
